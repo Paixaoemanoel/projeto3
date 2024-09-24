639f6ad44a9812f502f1dd0170a78818b61ca4a4
Paixaoemanoel <107117127+Paixaoemanoel@users.noreply.github.com>
2024-09-24 07:59:15 -0300

    projeto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programaçã'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Robótica',
+    'Qual foi o primeiro robô do mundo?',
+    'O barco teleoperado, similar a um ROV moderno, inventado por Nikolas Tesla com a exibição em 1898'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
